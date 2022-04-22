ce4f44a28d1a5464bd4a3c9ab1be88566128d159
AngelaEcho <zsr060540@163.com>
2022-04-22 19:54:20 -0400

    tracker with kalman filter

--- detector.py
+++ detector.py
@@ -4,16 +4,40 @@ import cv2
 from time import time
 
 
-class ObjectionDetection:
+class ObjectDetection:
     def __init__(self, in_file, out_file):
         self.model = self.load_model()
         self.classes = self.model.names
         self.in_file = in_file
         self.out_file = out_file
+        self.cap = cv2.VideoCapture(in_file)
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-    def get_video(self, path):
-        return cv2.VideoCapture(path)
+    def mouse_detect(self, frame):
+        x1, y1, x2, y2 = 0, 0, 0, 0
+
+        def on_mouse(event, x, y, flags, param):
+            global point1
+            if event == cv2.EVENT_LBUTTONDOWN:
+                point1 = (x, y)
+                cv2.circle(frame, point1, 10, (255, 0, 0), 5)
+                print("x:{}, y:{}".format(x, y))
+                x1, y1 = x, y
+            elif event == cv2.EVENT_MOUSEMOVE and (flags & cv2.EVENT_FLAG_LBUTTON):
+                cv2.rectangle(frame, point1, (x, y), (0, 255, 0), 15)
+            elif event == cv2.EVENT_LBUTTONUP:
+                point2 = (x, y)
+                cv2.rectangle(frame, point1, point2, (0, 0, 255), 5)
+                x2, y2 = x, y
+
+        cv2.imshow('frame', frame)
+        cv2.namedWindow('frame')
+        cv2.setMouseCallback('frame', on_mouse)
+
+        return x1, y1, x2, y2
+
+    def get_video(self):
+        return self.cap
 
     def load_model(self):
         model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
@@ -44,8 +68,22 @@ class ObjectionDetection:
 
         return frame
 
+    def detect(self, frame):
+        labels, cord = self.score_frame(frame)
+        n = len(labels)
+        x_shape, y_shape = frame.shape[1], frame.shape[0]
+        centers = []
+        for i in range(n):
+            row = cord[i]
+            if row[4] >= 0.2:
+                x1, y1, x2, y2 = int(row[0]*x_shape), int(row[1]*y_shape), int(row[2]*x_shape), int(row[3]*y_shape)
+                if self.class_to_label(labels[i]) == 'sports ball':
+                    centers.append(np.array([x1, y1, x2, y2]))
+
+        return centers
+
     def __call__(self):
-        player = self.get_video(self.in_file)
+        player = self.get_video()
         assert player.isOpened()
         x_shape = int(player.get(cv2.CAP_PROP_FRAME_WIDTH))
         y_shape = int(player.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -63,9 +101,10 @@ class ObjectionDetection:
             print(f"Frames Per Second: {fps}")
             out.write(frame)
 
-detection_ball = ObjectionDetection("ball.mp4", "ball_od.avi")
-detection_ball()
 
-detection_multi = ObjectionDetection("multiobj.avi", "multi_od.avi")
-detection_multi()
+# detection_ball = ObjectDetection("ball.mp4", "ball_od.avi")
+# detection_ball()
+#
+# detection_multi = ObjectDetection("multiobj.avi", "multi_od.avi")
+# detection_multi()
 
--- tracker.py
+++ tracker.py
@@ -1,33 +1,71 @@
 import cv2
+import numpy as np
+from detector import ObjectDetection
+from kalmanfilter import KalmanFilter
 
 
-cap = cv2.VideoCapture("ball.mp4")
+# in_file = "ball.mp4"
+# out_file = "ball_track.avi"
+in_file = "multiobj.avi"
+out_file = "multi_track.avi"
 
+od = ObjectDetection(in_file, out_file)
+cap = od.get_video()
 
-od = OrangeDetector()
+x_shape = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+y_shape = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+four_cc = cv2.VideoWriter_fourcc(*"MPEG")
+out = cv2.VideoWriter(out_file, four_cc, 20, (x_shape, y_shape))
 
-kf = KalmanFilter()
-
-
-
+# ret, frame = cap.read()
+# centers = od.detect(frame)
+# x1, y1, x2, y2 = centers[0]
+# a1, b1, a2, b2 = centers[1]
+# cx = int((x1 + x2) / 2)
+# cy = int((y1 + y2) / 2)
+# ca = int((a1 + a2) / 2)
+# cb = int((b1 + b2) / 2)
+# print(cx, cy, ca, cb)
+kf0 = KalmanFilter(0.1, 2, 161, 1, 0.1, 0.1)
+kf1 = KalmanFilter(0.1, 152, 621, 1, 0.1, 0.1)
+kfs = [kf0, kf1]
 
 while True:
     ret, frame = cap.read()
     if ret is False:
         break
 
-    orange_bbox = od.detect(frame)
-    x, y, x2, y2 = orange_bbox
-    cx = int((x + x2) / 2)
-    cy = int((y + y2) / 2)
+    centers = od.detect(frame)
+    for idx, center in enumerate(centers):
+        if idx >= 2:
+            continue
+        kf = kfs[idx]
+        x1, y1, x2, y2 = center
+        cx = int((x1 + x2) / 2)
+        cy = int((y1 + y2) / 2)
+        print(cx, cy)
+        # Draw
+        cv2.circle(frame, (cx, cy), 5, (255, 0, 0), -1)
 
-    predicted = kf.predict(cx, cy)
-    cv2.circle(frame, (cx, cy), 20, (0, 0, 255), -1)
-    cv2.circle(frame, (cx, cy), 20, (255, 0, 0), -1)
+        # Predict
+        (x, y) = kf.predict()
+        print(x, y)
 
-    cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1)
-    if key == 27:
-        break
+        # Update
+        cxy = np.array([[cx], [cy]])
+        (nx, ny) = kf.update(cxy)
+        print(nx, ny)
+        cx = int(nx[0,0])
+        cy = int(ny[0,0])
+
+        # Draw
+        cv2.circle(frame, (int(x[0,0]), int(y[0,0])), 10, (0, 255, 0), -1)
+        cv2.circle(frame, (int(nx[0,0]), int(ny[0,0])), 10, (0, 0, 255), -1)
+
+        out.write(frame)
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
 
 
